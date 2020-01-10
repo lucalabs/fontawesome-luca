78c6241b21f2218be5b0c9efe841d036d97bd0c8
Erik Axel Nielsen <erikaxel@lucalabs.com>
2020-01-10 13:15:46 +0100

    Use directories

---
+++ pro-regular-svg-icons/faPiano.js
@@ -0,0 +1,29 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+var prefix = 'far';
+var iconName = 'piano';
+var width = 512;
+var height = 512;
+var ligatures = [];
+var unicode = 'f8d4';
+var svgPathData = 'M476.62 270.31l-57.24-28.62A64 64 0 0 1 384 184.44C384 82.58 301.42 0 199.55 0h-15.1C82.58 0 0 82.58 0 184.44V480a32 32 0 0 0 32 32h448a32 32 0 0 0 32-32V327.55a64 64 0 0 0-35.38-57.24zM464 464H48v-80h40v24a16 16 0 0 0 16 16h16a16 16 0 0 0 16-16v-24h48v24a16 16 0 0 0 16 16h16a16 16 0 0 0 16-16v-24h48v24a16 16 0 0 0 16 16h16a16 16 0 0 0 16-16v-24h48v24a16 16 0 0 0 16 16h16a16 16 0 0 0 16-16v-24h40zm0-128H48V184.44C48 109.21 109.21 48 184.45 48h15.1C274.79 48 336 109.21 336 184.44a111.38 111.38 0 0 0 61.91 100.18l57.24 28.62a15.91 15.91 0 0 1 8.85 14.31z';
+
+exports.definition = {
+  prefix: prefix,
+  iconName: iconName,
+  icon: [
+    width,
+    height,
+    ligatures,
+    unicode,
+    svgPathData
+  ]};
+
+exports.faPiano = exports.definition;
+exports.prefix = prefix;
+exports.iconName = iconName;
+exports.width = width;
+exports.height = height;
+exports.ligatures = ligatures;
+exports.unicode = unicode;
+exports.svgPathData = svgPathData;
